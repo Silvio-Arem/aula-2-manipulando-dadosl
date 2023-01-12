eed224171e10aa9935630c64501c82ffe0b79158
Silvio Arem <silvio.arem@gmail.com>
2023-01-12 18:41:20 -0300

    lib used to verify the input name

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
 let btnEnviar = document.querySelector("#btnEnviar");
+let inputNome = document.getElementById("nome");
 toastr.options.positionClass = "toast-top-right";
 
 btnEnviar.onclick = () => {
+    if (!inputNome.value) {
+        return toastr.error("Dados inválidos!");
+    }
+
     toastr.success("Dados enviados com sucesso!");
-}
+};
 
 
