f77617be3b5fc3484fd63ca8bbaa687e217f9b6e
Silvio Arem <silvio.arem@gmail.com>
2023-01-12 19:24:58 -0300

    lib used to compare if one date is after another

--- src/js/date.js
+++ src/js/date.js
@@ -1,3 +1,5 @@
 let hoje = new Date();
 let cincoDias = dateFns.addDays(hoje, 5);
 console.log(dateFns.format(cincoDias, "DD/MM/YYYY"));
+
+console.log("É depois?", dateFns.isAfter(cincoDias, hoje))
