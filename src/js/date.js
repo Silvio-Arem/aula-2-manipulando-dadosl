2951a088f61e92b436667dba4339c622730bdccb
Silvio Arem <silvio.arem@gmail.com>
2023-01-12 19:26:57 -0300

    to format and show the hours and minutes too

--- src/js/date.js
+++ src/js/date.js
@@ -1,5 +1,5 @@
 let hoje = new Date();
 let cincoDias = dateFns.addDays(hoje, 5);
-console.log(dateFns.format(cincoDias, "DD/MM/YYYY"));
+console.log(dateFns.format(cincoDias, "DD/MM/YYYY HH:MM"));
 
 console.log("É depois?", dateFns.isAfter(cincoDias, hoje))
